d6d69c951f7438e0d2120d6178fbfe6a0e5ee3a5
orkan6 <158033955+orkan6@users.noreply.github.com>
2024-08-12 13:56:32 +0300

    Create extensions.py

---
+++ cs50/problem set 1/extensions.py
@@ -0,0 +1,20 @@
+file = input("File name: ").lower()
+if ".gif" in file:
+    print("image/gif")
+
+elif ".jpg" in file or ".jpeg" in file:
+    print("image/jpeg")
+
+elif ".png" in file:
+    print("image/png")
+
+elif ".pdf" in file:
+    print("application/pdf")
+
+elif ".txt" in file:
+    print("text/plain")
+
+elif ".zip" in file:
+    print("application/zip")
+else:
+    print("application/octet-stream")
